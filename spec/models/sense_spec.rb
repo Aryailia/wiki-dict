98068f094220fdaeae91498d6b689c0c714f0fe2
Aryailia <Aryailia@users.noreply.github.com>
2017-11-22 03:24:12 +0800

    changes

--- spec/models/sense_spec.rb
+++ spec/models/sense_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Sense, type: :model do
-	before(:each) do
-		@user = User.create!(email: 'kevin@example.com', password: '123456')
-	end
-
 	describe 'validations' do
 		it { is_expected.to validate_presence_of(:content) }
 		it { is_expected.to validate_presence_of(:word_class) }
